3c531294668fc8b687fc60d0b86443ef852f2ad7
Dylan <dylan@dylandover.com>
2020-06-28 22:47:49 -0500

    feat: inital commit (Card+CardHolder)

---
+++ src/Components/Card.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+const defaultCardProps = {
+  cardHeight: 300,
+}
+
+const generateDefaultContentProps = (cardHeight) => ({
+  style: {
+    maxHeight: cardHeight - 50,
+    overflow: "scroll",
+  },
+});
+
+const defaultCardStyle = {
+  backgroundColor: "white",
+  border: "0px solid #787878",
+  borderRadius: 25,
+  boxShadow: "0px 4px 10px #787878",
+  cardHeight: 300,
+}
+
+const defaultTitleProps = {
+  fontWeight: "bold",
+  color: "#383838",
+  letterSpacing: 2,
+}
+
+function Card({ children, cardHeight, id, isInitialTop, onClick, title, ...props }) {
+  const computedHeight = cardHeight || defaultCardProps.cardHeight;
+  const computedStyle = {
+    ...defaultCardStyle,
+    marginTop: isInitialTop ? 0 : -computedHeight + 50,
+    height: computedHeight,
+    ...props.style,
+  }
+  const defaultContentProps = generateDefaultContentProps(computedHeight);
+  return (
+    <div
+      className="card"
+      id={id}
+      onClick={onClick}
+      {...props}
+      style={computedStyle}
+    >
+      <p style={{ ...defaultTitleProps }}>{title}</p>
+      <div className="card-content" {...defaultContentProps}>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/Components/CardHolder.js
@@ -0,0 +1,71 @@
+import gsap from "gsap";
+import React, { useEffect, useState } from 'react';
+
+const defaultCardHolderProps = {
+  display: "flex",
+  flexDirection: "column",
+  left: 400,
+  position: "absolute", 
+  top: 100,
+  cardWidth: 250,
+};
+
+const lastElement = (arr) => arr[arr.length - 1];
+
+function range(start, end) {
+  return Array(end - start + 1).fill().map((_, idx) => start + idx)
+}
+
+const unit = 50;
+
+function CardHolder({ children, cardHeight, cardWidth, ...props }) {
+  const [cardOrder, setCardOrder] = useState(range(0, children.length - 1))
+  const [topIndex, setTopIndex] = useState(children.length - 1);
+
+  useEffect(() => {
+    if (lastElement(cardOrder) === topIndex) return;
+    document.getElementById(`c-${topIndex}`).style.zIndex = 999;
+    setTimeout(() => {
+      let haveSeenTopIndex = false;
+      cardOrder.forEach((e) => {
+        if (e === topIndex) {
+          gsap.to(`#c-${topIndex}`, 1, {y: (children.length - 1 - topIndex) * unit}); 
+          haveSeenTopIndex = true;
+        }
+        else if (haveSeenTopIndex) {
+          const targetPosition = cardOrder.indexOf(e) - 1;
+          const initialPosition = e;
+          const stepDelta = targetPosition - initialPosition;
+          const newOffset = unit * stepDelta;
+          gsap.to(`#c-${e}`, 1, {y: newOffset});
+        }
+      })
+    }, 300)
+    const newOrder = [...cardOrder.filter(c => c !== topIndex), topIndex];
+    newOrder.forEach((e, i) => {
+      document.getElementById(`c-${e}`).style.zIndex = i;
+    })
+    setCardOrder(newOrder);
+  }, [cardOrder, children, topIndex])
+
+  const computedWidth = cardWidth || defaultCardHolderProps.cardWidth
+
+  return (
+    <div style={{ ...defaultCardHolderProps, width: computedWidth, ...props}}>
+      {children.map((Card, i) => (
+        React.cloneElement(
+          Card,
+          {
+            id: `c-${i}`,
+            key: `${i}`,
+            cardHeight: cardHeight,
+            onClick: () => setTopIndex(i),
+            isInitialTop: i === 0,
+          },
+        )
+      ))}
+    </div>
+  );
+};
+
+export default CardHolder;
